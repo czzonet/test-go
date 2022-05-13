fd083c72375fe94969dabe482d652d680333b5dd
czzonet <czzonet@163.com>
2022-05-13 14:58:00 +0800

    docs: :memo: readme

---
+++ README.md
@@ -0,0 +1,26 @@
+# test-go
+
+go 语言的使用
+
+## 运用
+
+- [x] 闭包 函数返回函数
+- [x] go 协程 通道
+- [x] 结构体
+
+## 起步
+
+- go mod init example/foo
+- go run .
+- go build
+- go install
+- go fmt：格式化
+
+## 总结
+
+体验了 go 语言的差异与优势。
+
+## Reference
+
+- [Go 学习路线（2022） - 掘金](https://juejin.cn/post/7061980386640789540#comment)
+- [Go 语言入门教程，Golang 入门教程（非常详细）](http://c.biancheng.net/golang/)
--- main.go
+++ main.go
@@ -5,49 +5,46 @@ import (
 	"math"
 )
 
-
-
-type Circle struct{
+type Circle struct {
 	radius float64
 }
 
-func (c Circle) area()  float64{
-	return  3.14*c.radius * c.radius
+func (c Circle) area() float64 {
+	return 3.14 * c.radius * c.radius
 }
 
-func closure(ch chan int)  {
-  inc:= func () func()int  {
-		i:=0
-		return func ()int  {
+func closure(ch chan int) {
+	inc := func() func() int {
+		i := 0
+		return func() int {
 			i++
 			return i
 		}
 	}
 
 	c := inc()
-  fmt.Println(c())
-  fmt.Println(c())
-  fmt.Println(c())
+	fmt.Println(c())
+	fmt.Println(c())
+	fmt.Println(c())
 
-	ch<-9
+	ch <- 9
 }
 
 func main() {
-	var res =	fmt.Sprintf("test-%s","go")
-  fmt.Println(res)
+	var res = fmt.Sprintf("test-%s", "go")
+	fmt.Println(res)
 
-	getSquareRoot :=func (x float64)float64  {
+	getSquareRoot := func(x float64) float64 {
 		return math.Sqrt(x)
 	}
 
 	fmt.Println(getSquareRoot(9))
 
+	var c1 Circle
+	c1.radius = 10.00
+	fmt.Println(c1.area())
 
-  var	c1 Circle
-  c1.radius=10.00
-  fmt.Println(c1.area())
-
-	ch:=make( chan int)
+	ch := make(chan int)
 	go closure(ch)
-  <-ch
+	<-ch
 }
